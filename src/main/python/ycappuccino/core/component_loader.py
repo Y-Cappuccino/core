1e945cf7e91549a91f0a35a02f6b9e8bf0d8b4bc
aurelien.pisu <aurelien.pisu@gmail.com>
2024-05-26 23:15:20 +0200

    wip

---
+++ src/main/python/ycappuccino/core/component_loader.py
@@ -0,0 +1,3 @@
+#
+# component that allow to load pelix components, ycappuccino loader 
+#
---
+++ src/main/python/ycappuccino/core/ycappuccino_component_generator.py
@@ -0,0 +1,3 @@
+#
+# class that allow to generate from ycapuccino components pelix components in order to be loaded by the pelix framework
+#
