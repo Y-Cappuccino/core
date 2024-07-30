d891cefdd78a2f76bc9332d330d55b0e5e1ee78f
Aurelien Pisu <aurelien.pisu@araymond.com>
2024-07-30 10:31:02 +0200

    wip

--- src/unittest/python/test_component_loader.py
+++ src/unittest/python/test_component_loader.py
@@ -1,66 +1,9 @@
-import os
-
 import pytest
-
-from ycappuccino.api.core import YCappuccino
-from ycappuccino.core.adapters.component_discovery import FileComponentDiscovery
-from ycappuccino.core.adapters.fake_bundle_context import (
-    FakeBundleContext,
-    FakeModuleType,
-)
-from ycappuccino.core import framework
-from ycappuccino.core.api import ComponentDiscovered
 
 
 @pytest.mark.asyncio
-class TestComponentLoadery(object):
+class TestComponentLoader(object):
 
     @pytest.fixture(autouse=True)
     def setup(self):
-        self.discovery = F()
-        self.discovery.path = os.getcwd() + "/test_component_discovery"
-        self.discovery.context = FakeBundleContext()
-        self.framework = framework.get_framework()
-        self.framework.bundle_prefix = ["ycappuccino"]
-
-    @pytest.mark.parametrize(
-        "component_discovered",
-        [
-            [
-                ComponentDiscovered(
-                    module_name="list_components",
-                    module=FakeModuleType("list_components"),
-                    path=os.getcwd() + "/test_component_discovery/list_components.py",
-                ),
-                ComponentDiscovered(
-                    module_name="activity_logger",
-                    module=FakeModuleType("activity_logger"),
-                    path=os.getcwd() + "/test_component_discovery/activity_logger.py",
-                ),
-                ComponentDiscovered(
-                    module_name="configuration",
-                    module=FakeModuleType("configuration"),
-                    path=os.getcwd() + "/test_component_discovery/configuration.py",
-                ),
-            ]
-        ],
-    )
-    async def test_given_path_when_discover_then_component_discovered(
-        self, component_discovered
-    ):
-        # Given
-        await self.discovery.discover(self.discovery.path)
-        # When
-        assert len(self.framework.component_repository.components) == 3
-        for component_assert in component_discovered:
-            # then
-            component_discovered = await self.framework.component_repository.get(
-                component_assert.module_name
-            )
-            assert component_discovered.module_name == component_assert.module_name
-            assert (
-                component_discovered.module.__name__ == component_assert.module.__name__
-            )
-            assert component_discovered.path == component_assert.path.replace(
-                os.sep, "/"
-            )
+        pass
