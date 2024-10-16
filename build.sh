ef07b9b37449bb265df19aaea4dbfdc62fcfb33e
Aurelien Pisu <aurelien.pisu@araymond.com>
2024-10-16 13:32:12 +0200

    wip

--- build.sh
+++ build.sh
@@ -6,5 +6,14 @@ pip install pybuilder
 pip install twine
 
 pyb publish
-
-twine upload  target/dist/core*/dist/core*.whl --repository-url https://nexus.ycappuccino.fr/simple/
+if [ -z "$1" ]
+then
+    echo "No username supplied"
+    exit 0
+fi
+if [ -z "$2" ]
+then
+    echo "No password supplied"
+    exit 0
+fi
+twine upload  target/dist/core*/dist/core*.whl --repository-url https://$1:$2@nexus.ycappuccino.fr/simple/
