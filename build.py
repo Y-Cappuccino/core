1149853edd244b9e1af584b5860b9ded188420ce
Aurelien Pisu <aurelien.pisu@araymond.com>
2024-10-16 16:02:41 +0200

    wip

--- build.py
+++ build.py
@@ -7,7 +7,6 @@ from pybuilder.core import depends, task, use_plugin, init
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.flake8")
-use_plugin("python.coverage")
 use_plugin("python.distutils")
 
 
@@ -19,34 +18,3 @@ default_task = "publish"
 def set_properties(project):
     project.set_property("core", False)  # default is True
     project.depends_on_requirements("requirements.txt")
-
-
-@task("bundle_dependencies")
-@depends("prepare")
-def bundle_dependencies(project, logger):
-    # Path to where dependencies are installed during build
-    dependencies_dir = os.path.join(project.expand_path("$dir_target"), "dependencies")
-    os.makedirs(dependencies_dir, exist_ok=True)
-
-    # Copy all dependencies to the dependencies directory
-    for dep in project.dependencies:
-        dep_path = dep.project_file_path
-        if dep_path and os.path.exists(dep_path):
-            if os.path.isdir(dep_path):
-                shutil.copytree(
-                    dep_path, os.path.join(dependencies_dir, os.path.basename(dep_path))
-                )
-            else:
-                shutil.copy(dep_path, dependencies_dir)
-
-    # Include dependencies directory in the wheel
-    project.get_property("wheel_include_dirs").append("dependencies")
-
-
-use_plugin("python.install_dependencies")
-
-
-@init
-def additional_properties(project):
-    # Ensure the dependencies are included in the wheel
-    project.set_property("wheel_include_dirs", [])
--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ python -m venv .venv
 . .venv/bin/activate
 pip install pybuilder
 pip install twine
-pip install
+pip install --index-url https://yaiba:Danst0ncu1%21@nexus.ycappuccino.fr/ ycappuccino_api
 pyb publish
 if [ -z "$1" ]
 then
@@ -16,4 +16,4 @@ then
     echo "No password supplied"
     exit 0
 fi
-twine upload  target/dist/core*/dist/core*.whl --repository-url https://nexus.ycappuccino.fr/ -u$1 -p$2
+twine upload  target/dist/ycappuccino_core*/dist/ycappuccino_core*.whl --repository-url https://nexus.ycappuccino.fr/ -u$1 -p$2
